fbd297d35c2d36795901290a142ab2699c861cbe
SAHA Georges <wonepo@gmail.com>
2024-08-16 08:33:54 +0100

    🎨 F01 : LOGIN > Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // @ts-nocheck
 import { useState } from "react";
 
-import LoginForm from "./composant/LoginForm";
-import LoginPage from "./composant/LoginPage";
+import LoginForm from "./components/pages/login/LoginForm";
+import LoginPage from "./components/pages/login/LoginPage";
 
 function App() {
   const [count, setCount] = useState(0);
---
+++ src/components/pages/login/LoginPage.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "./LoginForm";
+import Logo from "../../Logo";
+
+function LoginPage() {
+  return (
+    <div>
+      <Logo />
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
